ae439f06542412b4e9a1caf0c45790bf542443f9
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-06-01 10:55:04 +0200

    Update dependencies

--- flow-typed/classnames.js
+++ flow-typed/classnames.js
@@ -1,5 +1,13 @@
-/* @flow */
+type $npm$classnames$Classes =
+  string |
+  {[className: string]: * } |
+  Array<string> |
+  false |
+  void |
+  null
 
 declare module 'classnames' {
-  declare function exports(name: any): string;
+  declare function exports(
+    ...classes: Array<$npm$classnames$Classes>
+  ): string;
 }
--- flow-typed/react.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-//import type { Component } from 'react';
-// This does currently not work
-
-declare module 'react-addons-shallow-compare' {
-  declare var exports:
-    (instance: Object, nextProps: ?Object, nextState: ?Object) => boolean;
-}
