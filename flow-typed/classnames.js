6a370d887203d3a1e82722d30553f1866229d650
Daniel Tschinder <daniel.tschinder@researchgate.net>
2017-07-15 19:23:41 +0200

    update libdef definitions

--- flow-typed/classnames.js
+++ flow-typed/classnames.js
@@ -1,13 +1,15 @@
 type $npm$classnames$Classes =
-  string |
-  {[className: string]: * } |
-  Array<string> |
-  false |
-  void |
-  null
+  | string
+  | { [className: string]: * }
+  | Array<string>
+  | false
+  | void
+  | null;
 
 declare module 'classnames' {
-  declare function exports(
-    ...classes: Array<$npm$classnames$Classes>
-  ): string;
+  declare function exports(...classes: Array<$npm$classnames$Classes>): string;
+}
+
+declare module 'classnames/bind' {
+  declare module.exports: $Exports<'classnames'>;
 }
