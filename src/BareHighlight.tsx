7f6222fef56cd1c4c5cf13f70196ae3e07a7fc8f
Daniel Tschinder <duck+github@mailbox.org>
2021-03-07 12:17:14 +0000

    feat: Replace classnames dependency with clsx

--- src/BareHighlight.tsx
+++ src/BareHighlight.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
+import cx from 'clsx';
 import type hljs from 'highlight.js';
 
 interface Props {
