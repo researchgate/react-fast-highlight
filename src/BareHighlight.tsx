8e22a6f1664ec9f829105c157799d6f77cc1b13a
Roman Makarov <deepspiral@yandex.ru>
2021-09-06 14:51:47 +0300

    fix lint

--- src/BareHighlight.tsx
+++ src/BareHighlight.tsx
@@ -67,7 +67,9 @@ export default class BareHighlight extends React.PureComponent<Props, State> {
     const { highlightjs, languages } = this.props;
     return new Promise((resolve) => {
       if (languages && languages.length === 1) {
-        resolve(highlightjs.highlight(this.getInitialCode(), { language: languages[0] }));
+        resolve(highlightjs.highlight(this.getInitialCode(), {
+          language: languages[0],
+        }));
       } else {
         resolve(highlightjs.highlightAuto(this.getInitialCode(), languages));
       }
